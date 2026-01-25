725e06ea1e3c4efd7ba0be051a60ad5737290de7
Sahil0912 <sahiljakhar.09122005@gmail.com>
2026-01-25 20:51:37 +0530

    Added attributes to ensure castling happens smoothly

--- src/game/Move.cpp
+++ src/game/Move.cpp
@@ -4,10 +4,24 @@ Move::Move(int start, int end){
     startSquare = start;
     endSquare = end;
     promotionPiece = Type::Empty;
+    oldRookSquare = -1;
+    newRookSquare = -1;
+    isCastling = false;
 }
 
 Move::Move(int start, int end, Type promotion){
     startSquare = start;
     endSquare = end;
     promotionPiece = promotion;
+    oldRookSquare = -1;
+    newRookSquare = -1;
+    isCastling = false;
+}
+Move::Move(int start, int end, int oldRook, int newRook, bool isCastle){
+    startSquare = start;
+    endSquare = end;
+    oldRookSquare = oldRook;
+    newRookSquare = newRook;
+    isCastling = isCastle;
+    promotionPiece = Type::Empty;
 }
--- src/game/Move.hpp
+++ src/game/Move.hpp
@@ -7,10 +7,14 @@ struct Move
     int startSquare;
     int endSquare;
     Type promotionPiece;
+    bool isCastling;
+    int oldRookSquare;
+    int newRookSquare;
     bool operator==(const Move &move) const{
-        return (startSquare == move.startSquare && endSquare == move.endSquare && promotionPiece == move.promotionPiece);
+        return (startSquare == move.startSquare && endSquare == move.endSquare && promotionPiece == move.promotionPiece && isCastling == move.isCastling && oldRookSquare == move.oldRookSquare && newRookSquare == move.newRookSquare);
     }
-    Move() : startSquare(0), endSquare(0), promotionPiece(Type::Empty){}
+    Move() : startSquare(0), endSquare(0), promotionPiece(Type::Empty), isCastling(false), oldRookSquare(-1), newRookSquare(-1){}
     Move(int start, int end);
     Move(int start, int end, Type promotion);
+    Move(int start, int end, int oldRook, int newRook, bool isCastle);
 };
